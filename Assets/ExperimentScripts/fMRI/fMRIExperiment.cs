a238c743699430e7054253d3a7fbe15c24be97c0
valesulpizio <valentinasulpizio@gmail.com>
2021-03-21 09:57:15 +0100

    Versione 1

---
+++ Assets/ExperimentScripts/fMRI/fMRIExperiment.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class fMRIExperiment : Experiment<fMRITrial>
+{
+    // User-settable values
+    [Header("Bus experiment setup")]
+
+    [Tooltip("Position of the camera at the beginning of the trial.")]
+    public float InitialX_CameraPosition = 0f;
+
+    [Tooltip("Position of the first bus on the road at the beginning of the trial.")]
+    public float InitialX_BusPosition = 355f;
+
+    // References to game objects
+    private GameObject BusLine;
+    private Rigidbody BusLineRigidBody;
+    private GameObject cameraObject;
+    private Rigidbody cameraRigidBody;
+
+    override protected IEnumerator InitializeExperiment()
+    {
+        // Get reference to bus
+        BusLine = GameObject.Find("BusLine");
+        BusLineRigidBody = BusLine.GetComponent<Rigidbody>();
+
+        // Get reference to camera
+        cameraObject = GameObject.Find("Camera");
+        cameraRigidBody = cameraObject.GetComponent<Rigidbody>();
+
+        // Initialize visibility
+        BusLine.SetActive(false);
+
+        yield return null;
+    }
+
+
+    override protected void DestroyExperiment()
+    {
+        BusLine.SetActive(true);
+    }
+
+
+    override protected IEnumerator StartExperiment()
+    {
+        yield return null;
+    }
+
+
+    override protected IEnumerator ConcludeExperiment()
+    {
+        yield return null;
+    }
+
+
+    override protected IEnumerator RunTrial(fMRITrial currentTrial)
+    {
+        // FIRST TRIAL PERIOD
+
+        // Reset camera position
+        cameraObject.transform.position = new Vector3(InitialX_CameraPosition, cameraObject.transform.position.y, 0);
+
+        BusLine.transform.position = new Vector3(InitialX_BusPosition, 0, 25);
+
+        // Set Structure        
+        BusLine.SetActive(true);
+
+        // Set camera and bus speed
+        cameraRigidBody.velocity = new Vector3(currentTrial.SM_velocity * 0.277778f, 0, 0);
+        BusLineRigidBody.velocity = new Vector3(currentTrial.OM_velocity * 0.277778f,0, 0);
+
+        // Save current time
+        currentTrial.StartTime = Time.time;
+
+        // Wait for TrialDuration seconds
+        yield return new WaitForSeconds(currentTrial.duration);
+
+        // END OF TRIAL
+
+        // Stop camera and bus, hide bus
+        BusLineRigidBody.velocity = new Vector3(0, 0, 0);
+        cameraRigidBody.velocity = new Vector3(0, 0, 0);
+        BusLine.SetActive(false);
+       
+        // Save current time
+        currentTrial.EndTime = Time.time;
+
+        // Wait a minimum intertrial time
+        yield return new WaitForSeconds(0);
+
+        // Wait for response within a timeout
+        yield return Response.Wait(currentTrial.ITI_duration - 0);
+
+        // Wait for the subject to release response keys
+        yield return Response.WaitRelease();
+
+        // Check if valid response
+        if (currentTrial.ResponseTime <= currentTrial.ChangeTime)
+            currentTrial.Valid = false;
+               
+    }
+
+
+    override protected void UpdateExperiment()
+    {
+    }
+}
---
+++ Assets/ExperimentScripts/fMRI/fMRITrial.cs
@@ -0,0 +1,48 @@
+﻿[System.Serializable]
+public class fMRITrial : Trial
+{
+    public int trial_id;
+    public string trial_type;
+    public string trial_code;
+    public int SM_velocity;
+    public int OM_velocity;
+    public string Condition_name;
+    public float onset;
+    public int duration;
+    public float ITI_duration;
+
+    public float StartTime = 0;
+    public float ChangeTime = 0;
+    public float EndTime;
+
+    // Define the header of the trial list file here
+    private static readonly string[] trialListHeader = { "trial_id", "trial_type", "trial_code", "SM_velocity", "OM_velocity", "Condition_name", "onset", "duration", "ITI_duration"};
+    override public string[] TrialListHeader => trialListHeader;
+
+    // Define the header of the results file here
+    private static readonly string[] resultsFileHeader = { "trial_id", "trial_type", "trial_code", "SM_velocity", "OM_velocity", "Condition_name", "onset", "duration", "ITI_duration", "StartTime", "ChangeTime" };
+    override protected string[] TrialSpecificResultsHeader => resultsFileHeader;
+
+    // Read takes an array of strings read from a row of the trial list and
+    // parses it into values of trial properties
+    override public void Read(string[] values)
+    {
+        trial_id = int.Parse(values[0]);
+        trial_type = values[1];
+        trial_code = values[2];
+        SM_velocity = int.Parse(values[3]);
+        OM_velocity = int.Parse(values[4]);
+        Condition_name = values[5];
+        onset = float.Parse(values[6]);
+        duration = int.Parse(values[7]);
+        ITI_duration = float.Parse(values[8]);
+    }
+
+    // Save must returns an array of values to be saved to the results file, one
+    // for each column of the results file as defined in ResultsFileHeader
+    override protected object[] Save()
+    {
+        ResponseTime = Response != 0 ? (ResponseTime - ChangeTime) * 1000 : 0;
+        return new object[] { "trial_id", "trial_type", "trial_code", "SM_velocity", "OM_velocity", "Condition_name", "onset", "duration", "ITI_duration", StartTime, ChangeTime };
+    }
+}
